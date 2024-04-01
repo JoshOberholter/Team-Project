aaa9a03048fff38589a68ee5b430560b11034d62
JoshOberholter <165290279+JoshOberholter@users.noreply.github.com>
2024-04-01 16:31:12 -0400

    Update DatabaseInt.java

--- DatabaseInt.java
+++ DatabaseInt.java
@@ -2,61 +2,80 @@ import java.util.ArrayList;
 
 public class Database {
     private ArrayList<User> participants;
-    private ArrayList<Message> messages;
+    private ArrayList<GroupChat> groupChats;
+    private static Object o = new Object();
+
 
     public Database() {
-        this.participants = null;
-        this.messages = null;
+        this.participants = new ArrayList<>();
+        this.groupChats = new ArrayList<>();
     }
 
-    public Database(ArrayList<User> participants, ArrayList<Message> posts) {
+    public Database(ArrayList<User> participants, ArrayList<GroupChat> groupChats) {
         this.participants = participants;
-        this.messages = posts;
+        this.groupChats = groupChats;
     }
 
-    public ArrayList<User> getParticipants() {
+    public ArrayList<User> getUsers() {
         return participants;
     }
 
-    public boolean addUser(User user) throws UserNotFoundException {
+    public boolean addUser(User user) {
         //adds a user if no other user has the same username
         try {
-            for (int i = 0; i < participants.size(); i++) {
-                if (this.participants.get(i).getUsername().equals(user.getUsername())) {
-                    throw new UserNotFoundException("Username already taken!");
+            for (User myUser : participants) {
+                if (user.getUsername().equals(myUser.getUsername())) {
+                    return false;
                 }
             }
-            this.participants.add(user);
-            return true;
-        } catch (NullPointerException e) {
-            this.participants.add(user);
+            synchronized (o) {
+                this.participants.add(user);
+            }
             return true;
         } catch (Exception e) {
-            e.printStackTrace();
             return false;
         }
+
     }
 
     public boolean removeUser(User user) throws UserNotFoundException {
         try {
             if (this.participants.contains(user)) {
-                this.participants.remove(user);
+                synchronized (o) {
+                    this.participants.remove(user);
+                }
                 return true;
             }
             throw new UserNotFoundException(String.format("%s is not a user.", user.getUsername()));
         } catch (NullPointerException e) {
             throw new UserNotFoundException(String.format("%s is not a user.", user.getUsername()));
         } catch (Exception e) {
-            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean addGroupChat(GroupChat groupChat) {
+        try {
+            synchronized (o) {
+                this.groupChats.add(groupChat);
+            }
+             return true;
+        } catch (Exception e) {
             return false;
         }
     }
 
-    
-
-    public ArrayList<Message> getMessages() {
-        return messages;
+    public boolean removeGroupChat(GroupChat groupchat) {
+        try {
+            if (this.groupChats.contains(groupchat)) {
+                synchronized (o) {
+                    this.groupChats.remove(groupchat);
+                }
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
     }
-
-
 }
