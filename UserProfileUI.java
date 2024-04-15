582422fea317a4102de3888600c22f30ac4045a6
surajpilla242 <164089032+surajpilla242@users.noreply.github.com>
2024-04-15 17:00:14 -0400

    Update UserProfileUI.java

--- UserProfileUI.java
+++ UserProfileUI.java
@@ -1,7 +1,15 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.io.File;
-
+/**
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2022 -- Project 5 -- Phase 2
+ *
+ * @author Joshia Oberholtz, Micheal Chen, Sonya Kraft, Suraj Pilla,  Purdue CS
+ * @version April 15th, 2024
+ *
+ * the portnumber is 215
+ */
 public class UserProfileUI extends JFrame {
     private User user; 
 
