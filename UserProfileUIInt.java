ba68117daba18d289545a5fd5ea610c583eea4fc
surajpilla242 <164089032+surajpilla242@users.noreply.github.com>
2024-04-15 17:00:35 -0400

    Update UserProfileUIInt.java

--- UserProfileUIInt.java
+++ UserProfileUIInt.java
@@ -1,5 +1,13 @@
 import java.io.File;
-
+/**
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2022 -- Project 5 -- Phase 2
+ *
+ * @author Joshia Oberholtz, Micheal Chen, Sonya Kraft, Suraj Pilla,  Purdue CS
+ * @version April 15th, 2024
+ *
+ * the portnumber is 215
+ */
 public interface UserProfileUIInt {
     void initializeGUI();
     void handlePhotoUpload(File selectedFile);
