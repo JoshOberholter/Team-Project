2ce7e37f6eb30769867ee1b94384f096159296f0
surajpilla242 <164089032+surajpilla242@users.noreply.github.com>
2024-04-15 16:55:15 -0400

    Update MessageInt.java

--- MessageInt.java
+++ MessageInt.java
@@ -1,3 +1,12 @@
+/**
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2022 -- Project 5 -- Phase 2
+ *
+ * @author Joshia Oberholtz, Micheal Chen, Sonya Kraft, Suraj Pilla,  Purdue CS
+ * @version April 15th, 2024
+ *
+ * the portnumber is 215
+ */
 public interface MessageInt {
     User getUser();
     String getMessage();
