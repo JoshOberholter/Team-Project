f8a8180e8197729fd083d0cf806f363a6a43cfb0
surajpilla242 <164089032+surajpilla242@users.noreply.github.com>
2024-04-15 16:53:27 -0400

    Update UserInt.java

--- UserInt.java
+++ UserInt.java
@@ -1,5 +1,14 @@
 import java.util.ArrayList;
-
+/**
+ * A server for the social media site.
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2022 -- Project 5 -- Phase 2
+ *
+ * @author Joshia Oberholtz, Micheal Chen, Sonya Kraft, Suraj Pilla,  Purdue CS
+ * @version April 15th, 2024
+ *
+ * the portnumber is 215
+ */
 public interface UserInt {
     String getUsername();
     String getPassword();
