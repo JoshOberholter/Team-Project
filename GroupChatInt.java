0b33b642f80bb0ff883164a1429459fd7235d42d
surajpilla242 <164089032+surajpilla242@users.noreply.github.com>
2024-04-15 16:54:35 -0400

    Update GroupChatInt.java

--- GroupChatInt.java
+++ GroupChatInt.java
@@ -1,6 +1,14 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-
+/**
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2022 -- Project 5 -- Phase 2
+ *
+ * @author Joshia Oberholtz, Micheal Chen, Sonya Kraft, Suraj Pilla,  Purdue CS
+ * @version April 15th, 2024
+ *
+ * the portnumber is 215
+ */
 public interface GroupChatInt {
 
     ArrayList<User> getParticipants();
