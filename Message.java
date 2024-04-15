4312ad6bdb9f4beacbb8acade7f708d29eb38009
Berrybusch <165302272+Berrybusch@users.noreply.github.com>
2024-04-15 12:25:58 -0400

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,23 +1,68 @@
-import java.lang.System;
+
+/**
+ * A class to store messages, the person who sent them, 
+ * if they have a photo, if they've been seen, then the time sent
+ *
+ * <p>Purdue University -- CS18000 -- Fall 2022 -- Project 5 -- Phase 2
+ *
+ * @author Joshia Oberholtz, Micheal Chen, Sonya Kraft, Suraj Pilla,  Purdue CS
+ * @version April 15th, 2024
+ *
+ */
 
 public class Message {
     private User user;
     private String message;
     private Boolean seen;
     private String photoPath;
+    private double time;
 
-    public Message(User user, String message, String photoPath) {
+    public Message(User user, String message, String photoPath, double time) {
         this.user = user;
         this.message = message;
         this.seen = false;
         this.photoPath = photoPath;
+        if (time == 0) {
+            this.time = System.currentTimeMillis();
+        } else {
+            this.time = time;
+        }
     }
 
-    public Message(User user, String message) {
+    public Message(User user, String message, double time) {
         this.user = user;
         this.message = message;
         this.seen = false;
         this.photoPath = "";
+        if (time == 0) {
+            this.time = System.currentTimeMillis();
+        } else {
+            this.time = time;
+        }
+    }
+
+    public Message(User user, String message, String photoPath, Boolean seen, double time) {
+        this.user = user;
+        this.message = message;
+        this.seen = seen;
+        this.photoPath = photoPath;
+        if (time == 0) {
+            this.time = System.currentTimeMillis();
+        } else {
+            this.time = time;
+        }
+    }
+
+    public Message(User user, String message, Boolean seen, double time) {
+        this.user = user;
+        this.message = message;
+        this.seen = seen;
+        this.photoPath = "";
+        if (time == 0) {
+            this.time = System.currentTimeMillis();
+        } else {
+            this.time = time;
+        }
     }
 
 
@@ -55,10 +100,14 @@ public class Message {
         return false;
     }
 
+    public double getTime() {
+        return time;
+    }
+
     public String toString() {
         String format = "";
 
-        format += String.format("#%s;%s;%b", this.user.getUsername(), this.message, this.seen);
+        format += String.format("#%s;%s;%b;%f", this.user.getUsername(), this.message, this.seen, this.time);
 
         if (this.photoPath.isEmpty()) {
             format += "#";
